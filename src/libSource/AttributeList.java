da0fe98a9590d173183a629465e46cdb4c75b651
Dayan <dayannn@yandex.ru>
2017-04-07 14:33:22 +0300

    Initial commit with basic classes by AntonNev

---
+++ src/libSource/AttributeList.java
@@ -0,0 +1,81 @@
+package libSource;
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class AttributeList
+{
+    private List<BaseAttribute> list = new ArrayList<BaseAttribute>();
+
+
+
+    public AttributeList()
+    {
+
+    }
+
+    public AttributeList(AttributeList attrList)
+    {
+        Iterator<BaseAttribute> itr = attrList.getIterator();
+        while (itr.hasNext())
+        {
+            list.add(itr.next());
+        }
+    }
+
+    public Iterator<BaseAttribute> getIterator()
+    {
+        return list.iterator();
+    }
+
+    public int size()
+    {
+        return list.size();
+    }
+
+    public  BaseAttribute get(int index)
+    {
+        return list.get(index);
+    }
+
+
+    public void add(BaseAttribute attribute)
+    {
+        list.add(attribute);
+    }
+
+    public void remove (int index)
+    {
+        list.remove(index);
+    }
+
+    public void set(int index, BaseAttribute attribute)
+    {
+        list.set(index, attribute);
+    }
+
+    public void setAttributeValue (int index, String value)
+    {
+        BaseAttribute attr = list.get(index);
+        attr.setAttributeValue(value);
+        list.set(index, attr);
+    }
+
+    public void setAttributeValue (String name, String value)
+    {
+        Iterator<BaseAttribute> itr = list.iterator();
+        BaseAttribute attr;
+        while(itr.hasNext())
+        {
+            attr = itr.next();
+            if (attr.getAttributeName().equals(name))
+            {
+                attr.setAttributeValue(value);
+                return;
+            }
+        }
+        return;
+    }
+}
---
+++ src/libSource/AttributeTheme.java
@@ -0,0 +1,13 @@
+package libSource;
+
+/**
+ * Created by Антон on 06.04.2017.
+ */
+public class AttributeTheme extends BaseAttribute
+{
+    public AttributeTheme(String _value)
+    {
+        value = _value;
+        name = "Theme";
+    }
+}
